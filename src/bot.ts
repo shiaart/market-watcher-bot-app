14c2c220bb6c7e9cc1104e48b970486eee06b6c4
Art Sh <shiabutdinov@gmail.com>
2020-05-24 13:00:28 +0300

    simple bot, dev proxy, scheduler
    Added simple reply bot, dev proxy for local run, simple sceduler

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,43 @@
 require('dotenv').config();
 import Telegraf from 'telegraf';
+import schedule from 'node-schedule';
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+let bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Dev mode, adding proxy');
+  const HttpsProxyAgent = require('https-proxy-agent');
+  const agent = new HttpsProxyAgent('http://45.132.18.106:8888');
+  const DefaultOptions = {
+    telegram: {
+      agent: agent,
+    },
+  };
+
+  bot = new Telegraf(process.env.TELEGRAM_TOKEN, DefaultOptions);
+}
 
 bot.start((ctx) => ctx.reply('Welcome'));
+
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.on('sticker', (ctx) => {
+  console.log(
+    'Message from: ' + ctx.message.chat.id + '; Content: ' + ctx.message.text
+  );
+  ctx.reply('Баян');
+});
+bot.hears('hi', (ctx) => {
+  console.log(
+    'Message from: ' + ctx.message.chat.id + '; Content: ' + ctx.message.text
+  );
+  ctx.reply('йо бро');
+});
+
+schedule.scheduleJob('30 * * * * *', function () {
+  bot.telegram.sendMessage(
+    -1001201523934,
+    'Здарова черти, я еще живой. как там на рынке?'
+  );
+  // bot.telegram.sendMessage('', 'Здарова черти, я еще живой. как там на рынке?');
+});
+
 bot.startPolling();
--- src/telegram.ts
+++ src/telegram.ts
@@ -1,16 +1,4 @@
-const HttpsProxyAgent = require('https-proxy-agent');
 import { Telegram } from 'telegraf';
 
-const DefaultOptions = {
-    apiRoot: 'https://api.telegram.org',
-    webhookReply: true,
-    agent: new HttpsProxyAgent({
-      host: process.env.PROXY_HOST,
-      port: process.env.PROXY_PORT,
-      keepAlive: true,
-      keepAliveMsecs: 10000
-    })
-  };
-
-const telegram = new Telegram(process.env.TELEGRAM_TOKEN, DefaultOptions);
+const telegram = new Telegram(process.env.TELEGRAM_TOKEN);
 export default telegram;
